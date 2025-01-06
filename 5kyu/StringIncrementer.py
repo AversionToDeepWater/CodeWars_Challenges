5452ed97d26697ed517dc3ce4130c0ae2a05057c
AversionToDeepWater <imanabdelgani@gmail.com>
2025-01-06 19:17:30 +0000

    Completed this faster than I thought I would! Apparently you can also use re library.

--- 5kyu/StringIncrementer.py
+++ 5kyu/StringIncrementer.py
@@ -18,5 +18,36 @@ foo099 -> foo100
 Attention: If the number has leading zeros the amount of digits should be considered.
 '''
 
-def increment_string(strng):
-    return strng
+def increment_string(string):
+    # start at one as we want to increase the number at the end by 1, or add 1 at the end if there isn't a number at the end of the text
+    numbers = 1
+    text = ""
+    for i in string:
+        if i.isdigit():
+            numbers += int(i)
+        else:
+            text += i
+
+    string = text + str(numbers)
+
+    return string
+
+example1 = "foo1"
+example2 = "foobar"
+
+test_output1 = increment_string(example1)
+test_output2 = increment_string(example2)
+print(test_output1)
+print(test_output2)
+
+
+#Article I looked at
+# https://www.geeksforgeeks.org/python-splitting-text-and-number-in-string/
+
+# Code below only works if there is a space between the word and the numbers in the string
+# i.e., can split 12 in 'foo 12' but NOT in 'foo12'
+# example = 'foo12'
+# num = [int(n) for n in example.split() if n.isdigit()]
+#
+# print(num)
+
